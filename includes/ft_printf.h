aef71a7f3d756d96ef1dfa174548149f86c7fbc9
Nicolas FORAY <nforay@e1r8p11.42.fr>
2020-03-09 19:59:19 +0100

    Norm + cleanup

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:08:26 by nforay            #+#    #+#             */
-/*   Updated: 2020/03/09 19:41:09 by nforay           ###   ########.fr       */
+/*   Updated: 2020/03/09 19:58:36 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,12 @@ enum	e_state
 # define F_H		"h"
 # define F_L		"l"
 # define F_POINT	"."
-
 # define ALLCONV	"cspdiuxX%nfge"
-
 # define B_TEN		"0123456789"
 # define B_HEX		"0123456789abcdef"
 # define B_MHEX		"0123456789ABCDEF"
-
 # define NB_FLAG	11
 # define NB_CONV	13
-
 # define TRUE		1
 # define FALSE		0
 
@@ -114,70 +110,83 @@ typedef struct				s_state_machine
 	va_list					params;
 }							t_state_machine;
 
-typedef int		(*t_function)(char *, t_state_machine *);
+typedef int	(*t_function)(char *, t_state_machine *);
 
-int		ft_printf(char *format, ...);
-void	extract_aste(t_state_machine *m);
-void	print_perc(t_state_machine *m);
-void	print_conv(t_state_machine *m);
-void	print_conv_hh(t_state_machine *m);
-void	print_conv_ll(t_state_machine *m);
-void	print_conv_h(t_state_machine *m);
-void	print_conv_l(t_state_machine *m);
-void	print_conv_nolength(t_state_machine *m);
-void	print_conv_chr(t_state_machine *m);
-void	print_conv_ptr(t_state_machine *m);
-void	print_conv_uns(t_state_machine *m);
-void	print_conv_uns_hh(t_state_machine *m);
-void	print_conv_uns_ll(t_state_machine *m);
-void	print_conv_uns_h(t_state_machine *m);
-void	print_conv_uns_l(t_state_machine *m);
-void	print_conv_chr(t_state_machine *m);
-void	print_conv_int(t_state_machine *m);
-void	print_conv_int_hh(t_state_machine *m);
-void	print_conv_int_ll(t_state_machine *m);
-void	print_conv_int_h(t_state_machine *m);
-void	print_conv_int_l(t_state_machine *m);
-void	print_conv_str(t_state_machine *m);
-void	print_conv_n(t_state_machine *m);
-int		intlen(int n, int len);
-int		get_intlen(int n, int len);
-int		get_intlen_hh(signed char n, int len);
-int		get_intlen_ll(long long int n, int len);
-int		get_intlen_h(short int n, int len);
-int		get_intlen_l(long int n, int len);
-void	ft_printf_nbr_hhd_fd(signed char n, int size, t_state_machine *m);
-void	ft_printf_signed_char(signed char n, t_state_machine *m);
-void	ft_printf_int_h(short int n, t_state_machine *m);
-void	ft_printf_nbr_h_fd(short int n, int size, t_state_machine *m);
-void	ft_printf_int_l(long int n, t_state_machine *m);
-void	ft_printf_nbr_l_fd(long int n, int size, t_state_machine *m);
-void	ft_printf_int_ll(long long int n, t_state_machine *m);
-void	ft_printf_nbr_ll_fd(long long int n, int size, t_state_machine *m);
-void	ft_printf_int(int n, t_state_machine *m);
-void	ft_printf_nbr_int_fd(int n, int size, t_state_machine *m);
-int		ft_conv_base_h(t_state_machine *m, int len,
-			unsigned short int nbr, char *base);
-void	ft_printf_putnbr_base_h(t_state_machine *m,
-			unsigned short int nbr, char *base);
-void	ft_printf_width_uns_h(t_state_machine *m, int len);
-int		ft_conv_base_hh(t_state_machine *m, int len, unsigned char nbr,
-			char *base);
-void	ft_printf_putnbr_base_hh(t_state_machine *m,
-			unsigned char nbr, char *base);
-void	ft_printf_width_uns_hh(t_state_machine *m, int len);
-int		ft_conv_base_l(t_state_machine *m, int len,
-			unsigned long int nbr, char *base);
-void	ft_printf_putnbr_base_l(t_state_machine *m, unsigned long int nbr,
-			char *base);
-void	ft_printf_width_uns_l(t_state_machine *m, int len);
-int		ft_conv_base_ll(t_state_machine *m, int len, unsigned long long int nbr,
-			char *base);
-void	ft_printf_putnbr_base_ll(t_state_machine *m, unsigned long long int nbr,
-			char *base);
-void	ft_printf_width_uns_ll(t_state_machine *m, int len);
-int		ft_conv_base(t_state_machine *m, int len, unsigned int nbr, char *base);
-void	ft_printf_putnbr_base(t_state_machine *m, unsigned int nbr, char *base);
-void	ft_printf_width_uns(t_state_machine *m, int len);
+int							ft_printf(char *format, ...);
+void						extract_aste(t_state_machine *m);
+void						print_perc(t_state_machine *m);
+void						print_conv(t_state_machine *m);
+void						print_conv_hh(t_state_machine *m);
+void						print_conv_ll(t_state_machine *m);
+void						print_conv_h(t_state_machine *m);
+void						print_conv_l(t_state_machine *m);
+void						print_conv_nolength(t_state_machine *m);
+void						print_conv_chr(t_state_machine *m);
+void						print_conv_ptr(t_state_machine *m);
+void						print_conv_uns(t_state_machine *m);
+void						print_conv_uns_hh(t_state_machine *m);
+void						print_conv_uns_ll(t_state_machine *m);
+void						print_conv_uns_h(t_state_machine *m);
+void						print_conv_uns_l(t_state_machine *m);
+void						print_conv_chr(t_state_machine *m);
+void						print_conv_int(t_state_machine *m);
+void						print_conv_int_hh(t_state_machine *m);
+void						print_conv_int_ll(t_state_machine *m);
+void						print_conv_int_h(t_state_machine *m);
+void						print_conv_int_l(t_state_machine *m);
+void						print_conv_str(t_state_machine *m);
+void						print_conv_n(t_state_machine *m);
+int							intlen(int n, int len);
+int							get_intlen(int n, int len);
+int							get_intlen_hh(signed char n, int len);
+int							get_intlen_ll(long long int n, int len);
+int							get_intlen_h(short int n, int len);
+int							get_intlen_l(long int n, int len);
+void						ft_printf_nbr_hhd_fd(signed char n, int size,
+							t_state_machine *m);
+void						ft_printf_signed_char(signed char n,
+							t_state_machine *m);
+void						ft_printf_int_h(short int n, t_state_machine *m);
+void						ft_printf_nbr_h_fd(short int n, int size,
+							t_state_machine *m);
+void						ft_printf_int_l(long int n, t_state_machine *m);
+void						ft_printf_nbr_l_fd(long int n, int size,
+							t_state_machine *m);
+void						ft_printf_int_ll(long long int n,
+							t_state_machine *m);
+void						ft_printf_nbr_ll_fd(long long int n, int size,
+							t_state_machine *m);
+void						ft_printf_int(int n, t_state_machine *m);
+void						ft_printf_nbr_int_fd(int n, int size,
+							t_state_machine *m);
+int							ft_conv_base_h(t_state_machine *m, int len,
+								unsigned short int nbr, char *base);
+void						ft_printf_putnbr_base_h(t_state_machine *m,
+								unsigned short int nbr, char *base);
+void						ft_printf_width_uns_h(t_state_machine *m, int len);
+int							ft_conv_base_hh(t_state_machine *m, int len,
+							unsigned char nbr,
+								char *base);
+void						ft_printf_putnbr_base_hh(t_state_machine *m,
+								unsigned char nbr, char *base);
+void						ft_printf_width_uns_hh(t_state_machine *m, int len);
+int							ft_conv_base_l(t_state_machine *m, int len,
+								unsigned long int nbr, char *base);
+void						ft_printf_putnbr_base_l(t_state_machine *m,
+							unsigned long int nbr,
+								char *base);
+void						ft_printf_width_uns_l(t_state_machine *m, int len);
+int							ft_conv_base_ll(t_state_machine *m, int len,
+							unsigned long long int nbr,
+								char *base);
+void						ft_printf_putnbr_base_ll(t_state_machine *m,
+							unsigned long long int nbr,
+								char *base);
+void						ft_printf_width_uns_ll(t_state_machine *m, int len);
+int							ft_conv_base(t_state_machine *m, int len,
+							unsigned int nbr, char *base);
+void						ft_printf_putnbr_base(t_state_machine *m,
+							unsigned int nbr, char *base);
+void						ft_printf_width_uns(t_state_machine *m, int len);
 
 #endif
